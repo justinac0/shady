f2bf4f5d33bd67f501201a80d9adfcd521cd77a5
justinac0 <justinac.1999@gmail.com>
2022-12-29 12:31:37 +1000

    cleaned up main loop.

--- source/main.c
+++ source/main.c
@@ -14,23 +14,18 @@ int main(void) {
     // shady_load("examples/rtw1/vertex.glsl", "examples/rtw1/fragment.glsl");
 
     while (shady_is_open()) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glClearColor(0, 0, 0, 1);
-
         shady_update();
 
         shady_draw();
 
-        // double xpos, ypos;
-        // glfwGetCursorPos(glfwGetCurrentContext(), &xpos, &ypos);
-
         shady_ui_begin();
         shady_ui_char('A', 16, 16);
-        shady_ui_char('b', 16 + 9, 16);
-        shady_ui_char('c', 16 + 9 + 9, 16);
+        shady_ui_char('b', 25, 16);
+        shady_ui_char('c', 34, 16);
+        shady_ui_char('!', 43, 16);
         shady_ui_end();
 
-        glfwSwapBuffers(glfwGetCurrentContext());
+        shady_swapbuffers();
     }
 
     shady_ui_terminate();
